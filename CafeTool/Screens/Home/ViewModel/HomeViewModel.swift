8990c7ed403faab6498d21038ab6d52c6467f2e0
gkum4 <gustavokumasawa@gmail.com>
2024-04-27 18:57:40 -0300

    Fix published variable outside main thread

--- CafeTool/Screens/Home/ViewModel/HomeViewModel.swift
+++ CafeTool/Screens/Home/ViewModel/HomeViewModel.swift
@@ -7,20 +7,21 @@
 
 import SwiftUI
 
+@MainActor
 class HomeViewModel: ObservableObject {
-    
     @Published var coffees: [CoffeeDataModel] = []
     
     func fetchCoffees() async {
         await withCheckedContinuation { continuation in
             DispatchQueue.global().asyncAfter(deadline: .now() + 2) { [weak self] in
-                self?.coffees = [
-                    CoffeeDataModel(name: "Manifesto", grams: 250, gramsLeft: 50),
-                    CoffeeDataModel(name: "Municipal", grams: 500, gramsLeft: 200)
-                ]
+                DispatchQueue.main.async {
+                    self?.coffees = [
+                        CoffeeDataModel(name: "Manifesto", grams: 250, gramsLeft: 50),
+                        CoffeeDataModel(name: "Municipal", grams: 500, gramsLeft: 200)
+                    ]
+                }
                 continuation.resume()
             }
         }
     }
-    
 }
