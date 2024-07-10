2c506f5b6190d666421bce98e33ff3796ff35a1a
gkum4 <gustavokumasawa@gmail.com>
2024-07-10 19:56:31 -0300

    Update text

--- CafeTool/Sources/Screens/Home/View/HomeView.swift
+++ CafeTool/Sources/Screens/Home/View/HomeView.swift
@@ -18,7 +18,7 @@ struct HomeView: View {
     var body: some View {
         List {
             ForEach(viewModel.coffees) { coffee in
-                Text("\(coffee.name) - (quantidade: \(coffee.gramsLeft)g/\(coffee.grams)g")
+                Text("\(coffee.name) - (qnt: \(coffee.gramsLeft)g/\(coffee.grams)g")
             }
         }
         .task {
